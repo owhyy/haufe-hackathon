2f3e31cef4f2ad9c194d83b4523cc390c89ed0ea
owhyy <ionbabin123@mail.ru>
2024-10-26 12:30:04 +0300

    feat: add sqlalchemy & models

---
+++ db/models.py
@@ -0,0 +1,52 @@
+from sqlalchemy import Boolean, Date, ForeignKey, Integer, String
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+    relationship,
+)
+import datetime
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Party(Base):
+    __tablename__ = "party"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(40))
+    # TODO(ion): this could be an unsigned int; need to figure out a db-agnostic way to set it as this though
+    max_budget: Mapped[int] = mapped_column(Integer)
+    date: Mapped[datetime.date] = mapped_column(Date)
+    code: Mapped[str] = mapped_column(String(10))
+    users: Mapped[list["User"]] = relationship(
+        back_populates="party", cascade="all, delete-orphan"
+    )
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(40))
+    is_admin: Mapped[bool] = mapped_column(Boolean(), default=False)
+    party_id: Mapped[int] = mapped_column(ForeignKey("party.id"))
+
+    party: Mapped["Party"] = relationship(back_populates="users")
+    assigned_things: Mapped["Thing"] = relationship(
+        back_populates="responsible", cascade="all, delete-orphan"
+    )
+
+
+class Thing(Base):
+    __tablename__ = "thing"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    price: Mapped[int] = mapped_column(Integer)
+    name: Mapped[str] = mapped_column(String(40))
+    description: Mapped[str] = mapped_column(String(40))
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+
+    responsible: Mapped["User"] = relationship(back_populates="assigned_things")
--- main.py
+++ main.py
@@ -1,4 +1,36 @@
+import datetime
 from bottle import route, run, template
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from db.models import Base, Thing, User, Party
+
+
+def _create_a_party(s: Session):
+    admin = User(
+        username="jojo",
+        is_admin=True,
+    )
+    joe = User(
+        username="joe",
+    )
+    alcohol = Thing(
+        price=2000,
+        name="Alcohol",
+        description="Something to warm your soul",
+        responsible=joe,
+    )
+    party = Party(
+        name="halloween party 2024",
+        max_budget=4000,
+        date=datetime.date(2024, 10, 26),
+        code="aaa111",
+        users=[admin, joe],
+    )
+
+    s.add_all([admin, joe, alcohol, party])
+    s.commit()
 
 
 @route("/new-party")
@@ -12,4 +44,9 @@ def main_page():
 
 
 if __name__ == "__main__":
+    engine = create_engine("sqlite://", echo=True)
+    Base.metadata.create_all(engine)
+    with Session(engine) as s:
+        _create_a_party(s)
+
     run(host="localhost", port=8080, debug=True, reloader=True)
